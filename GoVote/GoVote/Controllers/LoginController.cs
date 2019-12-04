0e1aec82a14534412c459c6e8d39cbd08307c4e2
adechan <ade.enter@yahoo.com>
2019-12-04 11:13:32 +0200

    updated LoginController

--- GoVote/GoVote/Controllers/LoginController.cs
+++ GoVote/GoVote/Controllers/LoginController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using TryLogin.Data;
+using GoVote.Data;
 
 namespace GoVote.Controllers
 {
