94dd4d9287c4181e139539eb3cc5ba1639c0a0ac
adechan <ade.enter@yahoo.com>
2019-12-16 00:06:11 +0200

    added GetPartiesName request for Party DB

---
+++ GoVote/GoVote/Controllers/PartiesController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using GoVote.Data;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using GoVote.DTO;
+using System;
+
+namespace GoVote.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PartiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Party>> GetPartiesName()
+        {
+            var parties = await _mediator.Send(new GetParties());
+            if (parties == null)
+            {
+                return NotFound();
+            }
+            return Ok(parties);
+        }
+    }
+}
